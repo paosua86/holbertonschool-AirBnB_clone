4f60df9dc87571c259e38af5a3ef3a8c0a31b838
paosua86 <paolasuarezb1986@gmail.com>
2022-10-24 11:36:27 -0500

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,40 @@
 #!/usr/bin/python3
+""" BaseModel """
+from datetime import datetime
+import uuid
 
-import os
 
-print(os.listdir())
+class BaseModel():
+    """ BaseModel Class for future classes"""
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize objects """
+        if kwargs:
+            for key, value in kwargs.items:
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    continue
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ String representation """
+        dic = dict(self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, dic)
+
+    def save(self):
+        """ Update and save object """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ Return directory """
+        dic = dict(self.__dict__)
+        dic['__class__'] = self.__class__.__name__
+        dic['created_at'] = datetime.isoformat(dic['created_at'])
+        dic['updated_at'] = datetime.isoformat(dic['updated_at'])
+        return dic
