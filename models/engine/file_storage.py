06645cf2947e4c54772bb240f00d34fd62c0ad1c
paosua86 <paolasuarezb1986@gmail.com>
2022-10-24 17:20:11 -0500

    . fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,15 +19,15 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
-            key = obj.__class__.__name__ + obj.id
+            key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+        dic = {}
+        for key in self.__objects.keys():
+            dic[key] = self.__objects[key].to_dict()
         with open(self.__file_path, "w") as f:
-            dic = {}
-            for key in self.__objects.keys():
-                dic[key] = self.__objects[key].to_dict()
             json.dump(dic, f)
 
     def reload(self):
